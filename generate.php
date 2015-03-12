d4cc433fc6efab32399c1900887eb0e8d3f2515a
Reeze Xia <reeze@php.net>
2015-03-12 12:00:00 +0800

    Add checking for gd

--- generate.php
+++ generate.php
@@ -1,3 +1,10 @@
 <?php
 
 // This will be used to generate a git repo contains the text you want to say.
+
+
+$text = "I LOVE PHP";
+
+if (!function_exists("imagecreatetruecolor")) {
+	die("GD extension required");
+}
