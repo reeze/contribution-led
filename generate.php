f2cd6a6c3c4d3868fa755f059250d4900a6140dc
Reeze Xia <reeze@php.net>
2015-03-13 11:28:28 +0800

    Start write on paper

--- generate.php
+++ generate.php
@@ -8,3 +8,14 @@ $text = "I LOVE PHP";
 if (!function_exists("imagecreatetruecolor")) {
 	die("GD extension required");
 }
+
+// Github's dashboard size 10 x 10 per pixel
+// TODO width should not be fixed
+$img = imagecreatetruecolor("530", "70");
+$color = imagecolorallocate($im, 0, 0, 0);
+
+
+imagettftext($img, 70, 0, 0, 0, $color);
+
+imagepng($img);
+imagedestroy($img);
